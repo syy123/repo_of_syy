d8440c3f67977e832ea9a06421b1535fe8a14621
syy <751806050@qq.com>
2016-04-24 16:17:20 +0800

    delete Disable()

--- config.py
+++ config.py
@@ -49,7 +49,7 @@ class SliderBox():
         self.staticText = wx.StaticText(parent, id, textName)  #文本框
         self.slider = wx.Slider(parent, id, defaultValue, startValue, endValue, 
                                 style = wx.SL_HORIZONTAL|wx.SL_AUTOTICKS)
-        self.slider.Disable()
+        #self.slider.Disable()
         self.slider.SetTickFreq(precision, 1)
         self.textCtrl = wx.TextCtrl(parent, id, str(float(self.slider.GetValue())/divisor))
         self.textCtrl.SetEditable(False)
